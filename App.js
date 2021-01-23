4bf4a61ab55de152879ba8c13ae6a0f865b4ec3a
Rafaelsantossgaldino <rafaelsaantossgaldino2@gmail.com>
2021-01-23 00:03:20 -0300

    finalizando estilos

--- App.js
+++ App.js
@@ -29,11 +29,11 @@ export default function App() {
         onChange={()=>{}}
         />
         <TouchableOpacity style={styles.btnSubmit}>
-          <Text >Acessar</Text>
+          <Text style={styles.submitText}>Acessar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity>
-          <Text>Criar conta gratuita</Text>
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta gratuita</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
@@ -65,5 +65,23 @@ const styles = StyleSheet.create({
     fontSize:17,
     borderRadius: 7,
     padding: 10
+  },
+  btnSubmit:{
+    backgroundColor: '#35aaff',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7
+  },
+  submitText:{
+    color: '#fff',
+    fontSize:18,
+  },
+  btnRegister: {
+    marginTop: 15,
+  },
+  registerText:{
+    color: '#fff',
   }
 });
